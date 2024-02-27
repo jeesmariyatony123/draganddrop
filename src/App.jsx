e5fcc9752bd29c4bcb596b01a83ea9d213efd294
jeesmariyatony123 <jeestony123@gmail.com>
2024-02-27 14:16:18 +0530

    draganddrop v1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import DragDropImageUploader from './component/DragDropImageUploader'
+
+function App() {
+
+  return (
+    <>
+      <DragDropImageUploader />
+    </>
+  )
+}
+
+export default App
